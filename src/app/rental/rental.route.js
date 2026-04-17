ddd2cd22adf13cedd527966b8aaf352abfee3d72
Sakib Fakir <FakirSakib22232@gmail.com>
2026-04-18 01:47:24 +0800

    feat(produce) add model

--- src/app/rental/rental.route.js
+++ src/app/rental/rental.route.js
@@ -1,6 +1,47 @@
 
 
 
+import express from "express";
+import verifyToken from "../../middleware/verifyToken.js";
+import { verifyRole } from "../../middleware.js";
+import { rentalController } from "./rental.controller.js";
+
+const router = express.Router();
+
+router.post(
+  "/rentals",
+  verifyToken,
+  verifyRole("VENDOR"),
+  rentalController.createRentalPlot
+);
+
+router.get("/rentals", rentalController.getAllRentalPlot);
+router.get("/rentals/:id", rentalController.getSingleRentalPlot);
+
+router.patch(
+  "/rentals/:id",
+  verifyToken,
+  verifyRole("VENDOR"),
+  rentalController.updateRentalPlot
+);
+
+
+
+router.delete(
+  "/rentals/:id",
+  verifyToken,
+  verifyRole("VENDOR"),
+  rentalController.deleteRentalPlot
+);
+
+
+
+
+
+
+export const rentalRouter = router;
+
+
 
 
 
